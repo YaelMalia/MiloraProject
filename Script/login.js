8695d57342599d0af822bb0d461cee81ecc33df4
YaelMalia <72941843+YaelMalia@users.noreply.github.com>
2023-07-21 10:21:29 -0600

    Login Script Updated 210723

--- Script/login.js
+++ Script/login.js
@@ -16,7 +16,7 @@ function togglePasswordVisibility() {
     var password = document.getElementById("password").value;
   
     if ((username === "Malia" && password === "12345") || (username === "kubo" && password === "kuboks")) {
-      window.location.href = "/Mil/Paginas/Main.html";
+      window.location.href = "../Paginas/Main.html";
     } else {
       alert("Credenciales incorrectas. Por favor, inténtalo de nuevo.");
       document.getElementById("username").value = "";
