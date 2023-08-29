1d178d6d1c59efca25bc1034ad43f7090117be2e
Malia <yaelcruz417@gmail.com>
2023-08-29 13:56:01 -0600

    Update FormsScripts.js

--- Script/Formularios_Js/FormsScripts.js
+++ Script/Formularios_Js/FormsScripts.js
@@ -936,41 +936,24 @@ function Insert_Turno(){
 function AgregarProceso(){
     let P_diseno = $("#P_Diseno")[0].value;
     let P_orden = $("#P_orden")[0].value;
-    let orden = "MXVP-PRUEBA_PROCESOS";
-    let BuscarDatos = {
-        "diseno": diseno,
-        "orden_compra": orden
-    };
-    $.ajax({
-        type: 'POST',
-        url: '../Php_forms/Buscar_OrdenProcesos.php',
-        data: BuscarDatos,
-        async: false,
-        success: function(returning){
-            if(returning == "Nada"){
-                alertify.alert("Error", "No se encontraron datos");
+    let cantidadProd = $("#P_cantidad")[0].value;
+    
+    
+    if(P_diseno == null || P_diseno == ""){
+        alertify.alert("Aviso", "No se ha ingresado un diseño");
+    }else{
+        if(P_orden == null || P_orden == ""){
+            alertify.alert("Aviso", "No se ha ingresado una orden de compra");
+        }else{
+            if(cantidadProd == null || cantidadProd == "" || cantidadProd <=0){
+                alertify.alert("Aviso", "No se ha ingresado una cantidad de producción o no es válida");
             }else{
-                var today = new Date();
-                var date = today.toISOString().slice(0,10);
-                var time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
-                var dateTime = date + ' ' + time;
-                var ArrayDatos = returning.split(':');
-                let noOrden = ArrayDatos[0];
-                let Procesos = ArrayDatos[1];
-                alert("No orden: " + noOrden + " - Procesos: "+ Procesos + " - Fecha y hora: " + dateTime);
+                // Aquí inicia todo el quilombo para ingresar los procesos
 
-                //Insertar proceso
-                $.ajax({
-                    type: 'POST',
-                    url: '../Php_forms/Insertar_Nproceso.php',
-                    data: BuscarDatos,
-                    async: false,
-                    success: function(returning){
 
-                    }
-                });
+
             }
         }
-    });
+    }
 }
 // --------------------------------- FIN TURNOS/CORTE TURNO ---------------------------------
