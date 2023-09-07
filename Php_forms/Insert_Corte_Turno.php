ccfe0059911e53eff10e9a7dd2cc85e5525f30ab
KuboKs <braska08@gmail.com>
2023-09-07 10:22:26 -0600

    Update Insert_Corte_Turno.php

--- Php_forms/Insert_Corte_Turno.php
+++ Php_forms/Insert_Corte_Turno.php
@@ -1,16 +1,10 @@
 <?php
     if(isset($_POST["ID_Orden"])){
-        $fechaInicio = $_POST["fechaInicio"];
-        $fechaLimite = $_POST["fechaLimite"];
-        $Orden = $_POST["ID_Orden"];
-        $Cliente = $_POST["Cliente"];
-        $No_Dis = $_POST["No_diseno"];
-        $CantidadP = $_POST["CantidadPzs"];
 
         try {
             require_once("../conexionBD/Consultas.php");
             $miloraObj = MiloraClass::singleton();
-            $data = $miloraObj->;
+            $data = $miloraObj->insert_orden($fechaInicio, $fechaLimite, $Orden,$Cliente, $No_Dis, $CantidadP);
 
             echo "si";
         } catch (\Throwable $th) {
